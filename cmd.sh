af250688cc472aa02fbce66c75af316020670983
yanzhao <649038269@qq.com>
2018-01-14 11:42:14 +0800

    Update

--- cmd.sh
+++ cmd.sh
@@ -4,6 +4,7 @@ set -e
 echo "Running Production Server"
 PRO_DIR=$CONF_DIR'.settings.live'
 python manage.py collectstatic --noinput --settings=$PRO_DIR
-python manage.py makemigrations --noinput --settings=$PRO_DIR
+python manage.py makemigrations --noinput home --settings=$PRO_DIR
+python manage.py makemigrations --noinput  --settings=$PRO_DIR
 python manage.py migrate --noinput --settings=$PRO_DIR
 exec gunicorn --bind unix:/tmp/$PRO_DIR.zwidny.com.socket --env DJANGO_SETTINGS_MODULE=$PRO_DIR $CONF_DIR'.wsgi.live'
