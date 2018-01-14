dca126627a19f4bc9bed8b9da6c6eed936694cbe
yanzhao <649038269@qq.com>
2018-01-14 11:45:50 +0800

    Update

--- cmd.sh
+++ cmd.sh
@@ -7,4 +7,4 @@ python manage.py collectstatic --noinput --settings=$PRO_DIR
 python manage.py makemigrations --noinput home --settings=$PRO_DIR
 python manage.py makemigrations --noinput  --settings=$PRO_DIR
 python manage.py migrate --noinput --settings=$PRO_DIR
-exec gunicorn --bind unix:/tmp/$PRO_DIR.zwidny.com.socket --env DJANGO_SETTINGS_MODULE=$PRO_DIR $CONF_DIR'.wsgi.live'
+exec gunicorn --bind unix:/tmp/$CONF_DIR.zwidny.com.socket --env DJANGO_SETTINGS_MODULE=$PRO_DIR $CONF_DIR'.wsgi.live'
